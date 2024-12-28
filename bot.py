42082c44e33517bf5ad002d3b727522c36d83ef9
leejw0587 <90506657+leejw0587@users.noreply.github.com>
2024-12-28 21:41:44 +0900

    Remove error handler

--- bot.py
+++ bot.py
@@ -38,52 +38,12 @@ async def on_message(message: discord.Message) -> None:
 
 @bot.event
 async def on_command_error(context: Context, error) -> None:
-    if isinstance(error, commands.CommandOnCooldown):
-        minutes, seconds = divmod(error.retry_after, 60)
-        hours, minutes = divmod(minutes, 60)
-        hours = hours % 24
-        embed = discord.Embed(
-            title="Calm down!",
-            description=f"Retry after {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}",
-            color=discord.Color.yellow()
-        )
-        await context.send(embed=embed)
-    elif isinstance(error, commands.MissingPermissions):
-        embed = discord.Embed(
-            title="Error!",
-            description="You don't have permission for this command: `" + ", ".join(error.missing_permissions) + "`",
-            color=discord.Color.red()
-        )
-        await context.send(embed=embed)
-    elif isinstance(error, commands.MissingRole):
-        embed = discord.Embed(
-            title="Error!",
-            description=f"You don't have role for this command: <@&{error.missing_role}>",
-            color=discord.Color.red()
-        )
-        await context.send(embed=embed)
-    elif isinstance(error, commands.BotMissingPermissions):
-        embed = discord.Embed(
-            title="Error!",
-            description="Bot missing permission: `" + ", ".join(error.missing_permissions) + "`",
-            color=discord.Color.red()
-        )
-        await context.send(embed=embed)
-    elif isinstance(error, commands.CommandNotFound):
-        embed = discord.Embed(
-            title="Error!",
-            description="Unknown command!",
-            color=discord.Color.red()
-        )
-        await context.send(embed=embed)
-    else:
-        embed = discord.Embed(
-            title="Error!",
-            description=f"An unknown error occurred while executing the command\n`{error}`",
-            color=discord.Color.red()
-        )
-        await context.send(embed=embed)
-    raise error
+    embed = discord.Embed(
+        title="Error!",
+        description=f"An error occurred while executing the command\n`{error}`",
+        color=discord.Color.red()
+    )
+    return await context.send(embed=embed)
 
 
 async def load_cogs() -> None:
